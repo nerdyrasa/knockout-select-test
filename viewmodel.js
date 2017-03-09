e8509430b9659e5dcdf4cce34f6efec6e8f5593f
RB Isaacson <nerdyrasa@users.noreply.github.com>
2017-03-09 13:09:59 -0600

    Experimenting with css binding.

--- viewmodel.js
+++ viewmodel.js
@@ -5,12 +5,31 @@ var ViewModel = function (places) {
   self.places = places;
   self.selectedCategory = ko.observable();
   self.categories = ["Food", "Attraction"];
+  self.currentProfit = ko.observable(0);
 
   self.processClickOnListItem = function() {
     console.log("clicked list item");
+    console.log("this is ", this);
+    var place = this;
+    populateInfoWindow(place.marker, largeInfoWindow, place);
+
   };
 
+  // Evalutes to a positive value, so initially we apply the "profitPositive" class
+  self.profitStatus = ko.pureComputed(function() {
+    return this.currentProfit() < 0 ? "profitWarning" : "profitPositive";
+  }, self);
+
+  // Causes the "profitPositive" class to be removed and "profitWarning" class to be added
+  self.currentProfit(-50);
+
   self.filteredByCategory = ko.computed(function() {
+
+
+    if (largeInfoWindow){
+      largeInfoWindow.setMarker = null;
+      largeInfoWindow.marker = null;
+    }
 
     var results = this.places;
 
@@ -33,7 +52,9 @@ var ViewModel = function (places) {
 
 // if results are empty, ju
 
-
+    if (results) {
+      filterMap(this.places, results);
+    }
     return results;
 
   }, self);
